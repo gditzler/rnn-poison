b0c742b4c7969a26b82fbe8a2c14d5ebda3bdb67
Gregory Ditzler <gregory.ditzler@gmail.com>
2017-07-31 14:03:42 -0700

    no real change

--- experiments.sh
+++ experiments.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash 
 
-
-python ptb_word_lm.py --train=data/ptb.train.txt --valid=data/ptb.valid.txt --test=data/ptb.test.txt --model=small --seed=2 > ptb_small_original.csv
-python ptb_word_lm.py --train=data/ptb.train.txt --valid=data/ptb.valid.txt --test=data/ptb.test.txt --model=medium --seed=2 > ptb_medium_original.csv
+# baseline 
+python ptb_word_lm.py --train=data/ptb.train.txt --valid=data/ptb.valid.txt --test=data/ptb.test.txt --model=small --seed=2 > outputs/ptb_small_original.csv
+python ptb_word_lm.py --train=data/ptb.train.txt --valid=data/ptb.valid.txt --test=data/ptb.test.txt --model=medium --seed=2 > outputs/ptb_medium_original.csv
