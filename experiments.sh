682e6acbdae92472ae3d5d89b870d1c4e8cbf199
Gregory Ditzler <gregory.ditzler@gmail.com>
2017-08-25 17:23:01 -0700

    init commit

--- experiments.sh
+++ experiments.sh
@@ -1,5 +1,32 @@
 #!/usr/bin/env bash 
 
-# baseline 
-python ptb_word_lm.py --train=data/ptb.train.txt --valid=data/ptb.valid.txt --test=data/ptb.test.txt --model=small --seed=2 > outputs/ptb_small_original.csv
-python ptb_word_lm.py --train=data/ptb.train.txt --valid=data/ptb.valid.txt --test=data/ptb.test.txt --model=medium --seed=2 > outputs/ptb_medium_original.csv
+
+
+
+
+# run the poison free data sets 
+python rnn_lm.py \
+  --model small \
+  --seed 42 \
+  --train=data/ptb.train.txt \
+  --test=data/ptb.test.txt \
+  --valid=data/ptb.valid.txt \
+  --output=outputs/ptb_exp_small_clean.csv
+
+python rnn_lm.py \
+  --model medium \
+  --seed 42 \
+  --train=data/ptb.train.txt \
+  --test=data/ptb.test.txt \
+  --valid=data/ptb.valid.txt \
+  --output=outputs/ptb_exp_small_clean.csv
+
+python rnn_lm.py \
+  --model large \
+  --seed 42 \
+  --train=data/ptb.train.txt \
+  --test=data/ptb.test.txt \
+  --valid=data/ptb.valid.txt \
+  --output=outputs/ptb_exp_small_clean.csv
+
+
