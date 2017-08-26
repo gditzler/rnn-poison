e33ce0016175ded57613a32e59de28e4a1d3277d
Gregory Ditzler <gregory.ditzler@gmail.com>
2017-08-25 18:25:28 -0700

    updated for exps

--- experiments.sh
+++ experiments.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash 
 
 
-
-
+seed=42
 
 # run the poison free data sets 
 python rnn_lm.py \
   --model small \
-  --seed 42 \
+  --seed ${seed} \
   --train=data/ptb.train.txt \
   --test=data/ptb.test.txt \
   --valid=data/ptb.valid.txt \
@@ -15,18 +14,18 @@ python rnn_lm.py \
 
 python rnn_lm.py \
   --model medium \
-  --seed 42 \
+  --seed ${seed} \
   --train=data/ptb.train.txt \
   --test=data/ptb.test.txt \
   --valid=data/ptb.valid.txt \
-  --output=outputs/ptb_exp_small_clean.csv
+  --output=outputs/ptb_exp_medium_clean.csv
 
 python rnn_lm.py \
   --model large \
-  --seed 42 \
+  --seed ${seed} \
   --train=data/ptb.train.txt \
   --test=data/ptb.test.txt \
   --valid=data/ptb.valid.txt \
-  --output=outputs/ptb_exp_small_clean.csv
+  --output=outputs/ptb_exp_large_clean.csv
 
 
